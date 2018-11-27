620e0a2d4c15da4cddbfedeed0310ad70447febd
Kalpana <kalpana.kolla@gmail.com>
2018-11-27 16:27:45 -0500

    Adding project

---
+++ Yummy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yummy.Startup))]
+namespace Yummy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
